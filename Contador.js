82062d204e911a7dd5cc77598faf12f833bd7bba
Drenghist <59576956+Drenghist@users.noreply.github.com>
2021-08-04 11:25:33 +0200

    Add files via upload

--- Contador.js
+++ Contador.js
@@ -33,7 +33,8 @@ function enviarDatos() {
   //instrucción para enviar los datos
   wipe();
   //alert(`hola ${$("#input1").val()}`)
-  $.post('http://drenghist.synology.me:8080/baterias/index.php', `origen=oriapp&destino=destapp&carretilla=carrapp&usuario=usuapp&obs=obsapp`);
+  //$.post('http://drenghist.synology.me:8080/baterias/index.php', `origen=oriapp&destino=destapp&carretilla=carrapp&usuario=usuapp&obs=obsapp`);
+  $("#formulario").submit();
 }
 
 function wipe() {
